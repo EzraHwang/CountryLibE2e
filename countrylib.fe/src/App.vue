8332c73e4585107d8df892a9fdd0c5748783f3a1
Ezra <e1z2r3a4@gmail.com>
2023-12-24 22:51:16 +0700

    Handle BE logic and can get data from Api by http

--- countrylib.fe/src/App.vue
+++ countrylib.fe/src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <template>
+  <div>
+    <h1>國家目錄</h1>
+    <ul>
+      <li v-for="country in countries" :key="country.name">
+        {{ country.name }}
+      </li>
+    </ul>
+  </div>
+</template>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import axios from "Axios";
 
 @Options({
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      countries: <Country[]>([]),
+    }
+  },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  countries!: Country[];
+  async mounted() {
+    const response = await axios.get("http://localhost:5074/CountryLib/Get");
+    this.countries = response.data;
+  }
+}
+
+interface Country {
+  name: string;
+  capital: string;
+  region: string;
+}
 </script>
 
 <style>
